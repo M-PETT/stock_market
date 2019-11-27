b2a173c610c7c61a20d60ff7d0dd44894440266a
M-PETT <matteo.pettena@student.h-farm.com>
2019-11-27 11:32:31 -0500

    document_the_code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from stock import get_price
 
+'''
+once imported the stock function we difene the output sentences that the program will return when used for Apple and google.
+'''
+
 n,price=get_price("AAPL")
 print("Company {} has a stock value of {}$".format(price, n))
 n,price=get_price("GOOGL")
--- my_package/stock.py
+++ my_package/stock.py
@@ -4,7 +4,10 @@ import json
 
 fmp_URL = 'https://financialmodelingprep.com/api/v3/company/profile/%s'
 
-def get_price(company="AAPL"):
+'''
+Now that we imported the required libraries , we define the function. This works through data taken from the fmp_URL link and it returns the name of the company selected and its stock price. 
+'''
+def get_price(company="AAPL"):             
     r = requests.get(fmp_URL % company)
     data = json.loads(r.text)
     price = data['profile']['price']
