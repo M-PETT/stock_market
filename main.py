61cfcbc084c3d4f14c5743ea23329786e1d4a801
Lorenzo Masarin <lorenzo.masarin@student.h-farm.com>
2019-11-27 17:12:11 +0100

    make repo in python package

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from stock import get_price
+from my_package import stock
 
-n,price=get_price("AAPL")
+n,price=stock.get_price("AAPL")
 print("Company {} has a stock value of {}$".format(price, n))
-n,price=get_price("GOOGL")
+n,price=stock.get_price("GOOGL")
 print("Company {} has a stock value of {}$".format(price, n))
 
 
