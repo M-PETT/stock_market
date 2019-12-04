837b047a66f1d49240a5bdd9d158564a8336e966
IvoryTower48 <torresan8@gmail.com>
2019-12-04 10:24:46 -0500

    Implementing argparse in main.py

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from my_package import stock
 import sys
 import time
+import argparse
 
-if len(sys.argv)>2:
-    n,price=stock.get_price(sys.argv[1])
+valid_firms = ["AAPL", "GOOGL"]
+
+def parsing_input():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("stock_code",
+                        help= "The ticker symbol of the company.",
+                        choices= valid_firms)
+    parser.add_argument("stock_code2",
+                        help= "The ticker symbol of the company.",
+                        choices= valid_firms)
+    parser.add_argument("-v", 
+                        help= "Increases verbosity of the program.",
+                        action= "store_true")
+    args = parser.parse_args()
+    return args
+
+try:
+    args= parsing_input()
+    n, price= stock.get_price(args.stock_code)
     print("Company {} has a stock value of {}$".format(price, n))
-    n,price=stock.get_price(sys.argv[2])
+    n, price= stock.get_price(args.stock_code2)
     print("Company {} has a stock value of {}$".format(price, n))
-else:
-    print("Give me 2 arguments as input!")
+    time.sleep(5)
     exit()
 
-
+except:
+    if len(sys.argv)>2:
+        print("Write correctly the arguments!")
+        exit()
+    else:
+        print("Write all the required arguments!")
+        exit()
