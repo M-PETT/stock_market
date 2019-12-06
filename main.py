af887bb6f1af5f0a49dc37590a529d2e0aa0888f
Lorenzo Masarin <lorenzo.masarin@student.h-farm.com>
2019-12-06 11:28:28 +0100

    verbosity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import argparse
 
 valid_firms = ["AAPL", "GOOGL"]
-
+# this function requires two positional arguments to identify the companies and an optional argument that allow the user to get more explicit information about the program"
 def parsing_input():
     parser = argparse.ArgumentParser()
     parser.add_argument("stock_code",
@@ -15,19 +15,28 @@ def parsing_input():
                         choices= valid_firms)
     parser.add_argument("-v", 
                         help= "Increases verbosity of the program.",
-                        action= "store_true")
+                        action= "store_true", default= False)
     args = parser.parse_args()
     return args
 
+#the following code gives the value of the stock price of the input companies
 try:
     args= parsing_input()
     n, price= stock.get_price(args.stock_code)
-    print("Company {} has a stock value of {}$".format(price, n))
-    n, price= stock.get_price(args.stock_code2)
-    print("Company {} has a stock value of {}$".format(price, n))
-    time.sleep(5)
-    exit()
+    if args.v:
+       print("Successfully fetched data")
+       print("Company {} has a stock value of {}$".format(price, n))
+    else:
+        print("{} = {}$".format(price, n))
 
+    n, price= stock.get_price(args.stock_code2)
+    if args.v:
+       print("Successfully fetched data")
+       print("Company {} has a stock value of {}$".format(price, n))
+    else:
+        print("{} = {}$".format(price, n))
+
+#this code helps the user to check if he wrote correctly the inputs
 except:
     if len(sys.argv)>2:
         print("Write correctly the arguments!")
